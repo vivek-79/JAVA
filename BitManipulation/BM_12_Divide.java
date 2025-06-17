0a0b597d32d2fa690ed55be17a0b37bc90585a5a
vivek-79 <vivekroy5667@gmail.com>
2025-06-17 10:04:44 +0530

    P_12

--- BitManipulation/BM_12_Divide.java
+++ BitManipulation/BM_12_Divide.java
@@ -4,5 +4,41 @@ public class BM_12_Divide {
     
     public static void main(String[] args) {
         
+        int dividend = 5;
+        int divisor =3;
+
+        if(divisor == dividend){
+            System.out.println(1);
+        };
+
+        long d = Math.abs(divisor);
+        long n = Math.abs(dividend);
+
+        Boolean sign = true;
+
+        if(divisor< 0 && dividend >=0 ) sign = false;
+        if(dividend <0 && divisor >=0) sign = false;
+        long quotient =0;
+
+        while( n >= d){
+            int cnt =0;
+            while( n >= (d << (cnt+1))){
+                cnt +=1;
+            };
+
+            quotient += 1<< cnt;
+            n -= (d<< cnt);
+        };
+
+        if(quotient  == (1<<31) && sign){
+
+            System.out.println("Greater than Int Max");
+        }
+        if(quotient == (1<<31) && !sign){
+            System.out.println("Smaller than Int Min");
+        }
+
+        quotient = sign ? quotient : (-1*quotient);
+        System.out.println("Ans is: "+ quotient);
     }
 }
