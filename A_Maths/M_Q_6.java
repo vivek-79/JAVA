4ad990ba0a59e26b09dfe6136ecea2c63983a53f
vivek-79 <vivekroy5667@gmail.com>
2025-10-12 02:51:20 +0530

    find non-repeating

--- A_Maths/M_Q_6.java
+++ A_Maths/M_Q_6.java
@@ -1,5 +1,49 @@
 package A_Maths;
 
+// find the number which is not repeating 3 times
+
 public class M_Q_6 {
-    
+
+    public static void main(String[] args) {
+
+        int arr[] = { 1, 2, 3, 1, 2, 3, 1, 2, 3, 99 };
+
+        System.out.println(findUnique(arr));
+    }
+
+    // Helper function
+    private static int findUnique ( int [] arr){
+
+
+        // Array of size 31
+        int container [] = new int[32];
+
+        // adding set bit to array indexes
+        for( int num:arr){
+
+            for( int i=0;i<32;i++){
+
+                if( (num & (1<< i)) !=0){
+                    container [i] ++;
+                }
+            }
+        }
+
+        // modulating by 3- as everyone is 3 times
+        for( int i=0;i<32;i++){
+            container [i] %=3;
+        }
+
+        // re-constructing number
+        int res =0;
+        for( int i=0;i<32;i++){
+
+            if( container[i] !=0){
+
+                res |=  (1 << i);
+            }
+        }
+
+        return res;
+    }
 }
