865a7fe7723497d11519274681e60fc6b1b3e2f7
vivek-79 <vivekroy5667@gmail.com>
2025-11-13 00:19:46 +0530

    oop abstraction-2

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -1,34 +1,74 @@
 package OOPS;
 
 public class Abstraction {
-    
+
     public static void main(String[] args) {
-        
+
         Car alto = new Alto();
         alto.name("Alto Car");
+
+        Bike bike = new Bike();
+
+        bike.start();
     }
 }
 
-// abstract class
-abstract class Car{
+interface Engine {
 
-    abstract void name( String name);
+    static final int NUM_OF_TYRE = 2;
+
+    void start();
+
+    void stop();
 }
 
+interface Light {
 
-class Alto extends Car{
+    void color();
+
+    void range();
+}
+
+class Bike implements Engine,Light {
+    
+
+    public void start() {
+        System.out.println("Bike started");
+    };
+
+    public void stop(){
+
+    }
+
+    public void color(){
+
+    }
+
+    public void range(){
+
+    }
+
+}
+
+// abstract class
+abstract class Car {
+
+    abstract void name(String name);
+}
+
+class Alto extends Car {
 
     @Override
-    void name( String name){
-        
+    void name(String name) {
+
         System.out.println(name);
     }
 }
 
-class Bolero extends Car{
+class Bolero extends Car {
 
     @Override
-    void name( String name){
+    void name(String name) {
         System.out.println(name);
     }
 }
